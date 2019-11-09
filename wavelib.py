0dbfb234cc6f85fa955f5599d7fa32ecf073d6f5
Hedonist <originalhedonist@gmail.com>
2019-11-09 12:15:02 +0000

    add project files, refactor

--- wavelib.py
+++ wavelib.py
@@ -1,6 +1,10 @@
-import wave, logging, sys, struct, math
+import logging
+import struct
+import sys
+import wave
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(message)s')
+
 
 def createmono(filename, length, wavefunction):
     sample = 44100
@@ -10,18 +14,19 @@ def createmono(filename, length, wavefunction):
     wf.setframerate(sample)
     wf.setnframes(N)
     wf.setnchannels(1)
-    try:    
-        for n in range(0,N):
+    try:
+        for n in range(0, N):
             t = length * n / N
             a = wavefunction(n, t, N)
             if a > 1 or a < -1:
                 raise Exception('wavefunction returned {} for n = {}, t = {}'.format(a, n, t))
-           
-            A = int(((a+1)*32767)-32768)
+
+            A = int(((a + 1) * 32767) - 32768)
             thebytes = struct.pack("<h", A)
             wf.writeframes(thebytes)
     finally:
         wf.close()
+
 
 def createstereo(filename, length, wavefunctionL, wavefunctionR):
     sample = 44100
@@ -31,19 +36,19 @@ def createstereo(filename, length, wavefunctionL, wavefunctionR):
     wf.setframerate(sample)
     wf.setnframes(N)
     wf.setnchannels(2)
-    try:    
-        for n in range(0,N):
+    try:
+        for n in range(0, N):
             t = length * n / N
             aL = wavefunctionL(n, t, N, 0)
             aR = wavefunctionR(n, t, N, 1)
-           
+
             if aL > 1 or aL < -1:
                 raise Exception('wavefunctionL returned {} for n = {}, t = {}'.format(a, n, t))
             if aR > 1 or aR < -1:
                 raise Exception('wavefunctionR returned {} for n = {}, t = {}'.format(a, n, t))
 
-            AL = int(((aL+1)*32767)-32768)
-            AR = int(((aR+1)*32767)-32768)
+            AL = int(((aL + 1) * 32767) - 32768)
+            AR = int(((aR + 1) * 32767) - 32768)
             thebytes = bytearray(struct.pack("<h", AL))
             wf.writeframes(thebytes)
             thebytes = bytearray(struct.pack("<h", AR))
--- wavetest.py
+++ wavetest.py
@@ -1,24 +1,25 @@
-import wave, logging, sys, struct, math
+import logging
+import struct
+import sys
+import wave
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(message)s')
 
-
 wf = wave.open('stereo1.wav', 'rb')
-logging.info('Channels: %d' , wf.getnchannels())
-logging.info('Sample width: %f' , wf.getsampwidth())
+logging.info('Channels: %d', wf.getnchannels())
+logging.info('Sample width: %f', wf.getsampwidth())
 logging.info('frame rate: %f', wf.getframerate())
 logging.info('n frames: %d', wf.getnframes())
 retL = []
 retR = []
-for n in range(0,5):
+for n in range(0, 5):
     thebytes = wf.readframes(1)
     decodedL = struct.unpack("<h", thebytes[0:2])
     decodedR = struct.unpack("<h", thebytes[2:4])
-    #print(decoded)
+    # print(decoded)
     retL.append(decodedL)
     retR.append(decodedR)
 print(retL)
 print(retR)
 
 wf.close()
-
